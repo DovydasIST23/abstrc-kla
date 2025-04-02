741157b6f8e0d45cc0a7ccedcc3a60205181ea92
DovydasIST23 <dovydas.bikauskas@stud.svako.lt>
2025-04-02 09:20:43 +0300

    v2

--- user-managment/index.php
+++ user-managment/index.php
@@ -4,8 +4,9 @@ require 'autoload.php';
 use App\Models\Admin;
 use App\Models\RegularUser;
 use App\Services\AuthService;
+use App\Core\Database;
 
-// Create an Admin user
+// Create an Admin user 
 $admin = new Admin("Alice", "alice@example.com", "admin123");
 
 // Create a Regular User
@@ -14,7 +15,10 @@ $user = new RegularUser("Bob", "bob@example.com", "user123");
 // Create AuthService
 $authService = new AuthService();
 
-// Admin Login
+// Use database
+$DB = new Database();
+
+// Admin Login 
 echo $authService->authenticate($admin, "alice@example.com", "admin123") . "<br>";
 
 // Regular User Login
